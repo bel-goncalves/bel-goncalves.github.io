8684ebbc2bf7482dbed8c1a558618253367f6052
Isabel Gonçalves <igl@cesar.school>
2024-01-10 22:10:50 -0300

    javascript scroll

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function descer(event){
+
+    event.preventDefault();
+
+    const link = event.currentTarget;
+    const idSecao = link.getAttribute("href").substring(1); // Remove o "#" do href
+    const secaoAlvo = document.getElementById(idSecao);
+
+    if(secaoAlvo){
+
+        const posicao = secaoAlvo.offsetTop;
+
+        window.scrollTo({
+            top: posicao,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function eventoDescer(link){
+    link.addEventListener("click", descer);
+}
+
+links.forEach(eventoDescer);
